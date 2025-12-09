89ece5e24a6288189355a870d6b8dab4aae66ece
EnriqueM07 <enriq.mart98@gmail.com>
2025-12-09 16:20:41 -0600

    Mod - Correccion Deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import cloudflare from "@astrojs/cloudflare";
+
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server", // OBLIGATORIO para endpoints
+  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   }
