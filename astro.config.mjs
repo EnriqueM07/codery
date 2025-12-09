3db64bd50600a7e51910eeb9a999b3b0a02c10d5
EnriqueM07 <enriq.mart98@gmail.com>
2025-12-09 15:52:06 -0600

    Quitar todo lo relacionado a _worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import cloudflare from "@astrojs/cloudflare";
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server", // OBLIGATORIO para endpoints
-  adapter: cloudflare(),
   vite: {
     plugins: [tailwindcss()]
   }
